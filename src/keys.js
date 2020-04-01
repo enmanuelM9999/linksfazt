f9c35244c5a3f0d8d06ee067f3c67c06fc5f2c74
enmanuelM9999 <48193636+enmanuelM9999@users.noreply.github.com>
2020-04-01 13:21:59 -0400

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@ module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || '',
-        database: process.env.DATABASE_NAME || 'db_links'
+        host: process.env.DATABASE_HOST || 'us-cdbr-iron-east-01.cleardb.net',
+        user: process.env.DATABASE_USER || 'b696788db440f3',
+        password: process.env.DATABASE_PASSWORD || '0fcfcb71',
+        database: process.env.DATABASE_NAME || 'heroku_a20e75802d7b786'
     }
 
 };
